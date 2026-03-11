0c155f01a4b294f1bbeb68c0fd75670da60eb06f
Madhav <202097667+madhavofficial@users.noreply.github.com>
2026-03-11 17:33:35 +0530

    Reorder imports (formatter)

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Student;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import com.example.demo.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,10 +1,11 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Student;
-import com.example.demo.repository.StudentRepository;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
 
 @Service
 public class StudentService {
